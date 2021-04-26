9c2d13e65bd88b209eea41cfd60a2a3eb68fe584
jaimedavid01 <jaimedavid01@yahoo.com>
2021-04-26 13:59:11 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from 'react';
+import Row from './Row'
+import requests from './requests';
+import Banner from './Banner';
+
+function App() {
+  return (
+    <div className="App">
+
+    <Banner />
+    <Row 
+    title="NETFLIX ORIINALS" 
+    fetchUrl={requests.fetchNetflixOriginals}
+    isLargeRow
+    />
+    <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+    <Row title="TopRated" fetchUrl={requests.fetchTopRated}/>
+    <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+    <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+    <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+    <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+    <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
+    </div>
+  );
+}
+
+export default App;
